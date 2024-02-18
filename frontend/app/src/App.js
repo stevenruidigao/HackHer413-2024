576ec0e5be10e9ea45f53719472770f58942441e
Archimeddes Li <escapereovery@gmail.com>
2024-02-17 23:31:44 -0500

    clicker game and post requests

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,17 +1,57 @@
+import React, { useState } from 'react';
+
 import './App.css';
 
 import Inventory from './Components/Menu/Inventory.js';
 import Menu from './Components/Menu/Menu.js'
+import ClickerGame from './Components/Minigame/ClickerGame.js'
 
 function App() {
+  const [minigame, setMinigame] = useState(true);
+  const [playerTurn, setPlayerTurn] = useState(true);
+  
+  const [attackPower, setAttackpower] = useState(0);
+  function incrPower(x) {
+    setAttackpower(attackPower + x);
+  }
+
+  //send data/receive data
+  function send(convId, userAction, name, scenario) {
+    console.log("SENDING THE USER INPUT UWUW: "+userAction)
+    let url = "/submit";
+    const response = fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      
+      body: JSON.stringify({
+        conversation_id: convId,
+        action: userAction,
+        name: name,
+        scenario: scenario
+      }),
+    }).then(data => data.json()).then(json => {
+      //update state
+      setAttackpower(99);
+      console.log("SERVER DATA RECEIVED")
+    });
+  }
+
+
   return (
     <div className="App">
       <div>
-        <Menu tab={1}/>
+        <Menu tab={1} send={(userAction)=>{send("", userAction, "arky", "The player is fighting against Kafka, a mind controlling fugitive who knows the secrets of the universe.")}}/>
         <h1>AIdventure</h1>
+
+        {minigame ? (<ClickerGame handleClick = {()=>{incrPower(0.5)}}/>) : (<></>)}
+  
       </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ frontend/app/src/Components/Minigame/ClickerGame.js
@@ -0,0 +1,19 @@
+import './Minigame.css'
+
+import target from './../../assets/target.png';
+
+function ClickerGame(props) {
+
+    return (
+        <div>
+            <h1>Click the target to deal damage!!</h1>
+            <div className="target-container">
+              <img src={target} className="target" onClick = {props.handleClick} alt=""></img>
+            </div>
+        </div>
+    )
+
+
+}
+
+export default ClickerGame;
