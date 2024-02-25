ac7c94dba47e8b606cc9c3cbbc83708b34b781b7
Archimeddes Li <escapereovery@gmail.com>
2024-02-24 23:37:43 -0500

    added continue

--- frontend/app/src/Components/Ending.js
+++ frontend/app/src/Components/Ending.js
@@ -4,18 +4,14 @@ import './Menu/Menu.css'
 function Ending(props) {
   
   return (
-    <div className="story">
 
 <div id="ending" class="overlay hidden">
   <div class="popup-content">
     <h1>Adventure Over!</h1>
     <p>Thank you for playing this AIdventure!</p>
+    <button class="popup-btn btn" onClick={() => {document.getElementById("ending").className = "overlay hidden"}}>Continue?</button>
   </div>
 </div>
-
-
-    </div>
-
   );
 }
 
